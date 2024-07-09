25a0038eaeecdf0e4a33c7573e219c15e197d6c4
Teddir <ferryaja20000@gmail.com>
2024-07-09 21:19:03 +0700

    fix: add hadist

---
+++ app/hadist/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/layout/navbar";
+import Image from "next/image";
+
+export default function Hadist() {
+  return (
+    <section className="w-full min-h-screen bg-white overflow-hidden">
+      <Navbar/>
+      home
+    </section>
+  );
+}
